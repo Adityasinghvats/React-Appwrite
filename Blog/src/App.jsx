f2209ca9fab2086b6b8f51e3b876fbb71bdefadd
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-11-18 18:44:12 +0530

    Set up appwrite , build collections , give permissions, create storage, build config for easy access

---
+++ Blog/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import conf from './conf/config'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>A blogging app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
